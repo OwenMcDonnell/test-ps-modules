c494d4f475ebef11800d7fba2327c7d4c318b5ad
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2024-12-03 16:17:51 -0800

    Create call_cmdlet.ps1

---
+++ call_cmdlet.ps1
@@ -0,0 +1,20 @@
+param(
+    [Parameter(Mandatory=$true)][String]$AzurePassword,
+    [Parameter(Mandatory=$true)][String]$AzureTenantId
+)
+
+try
+{
+    Write-Output "Connecting to Azure"
+    
+    $psCred = New-Object System.Management.Automation.PSCredential($AzureApplicationId, $azurePasswordSecureString)
+    $defaultProfile = Connect-AzAccount -Credential $psCred -TenantId $AzureTenantId  -ServicePrincipal
+
+    Write-Output "Connected to Azure, getting the cloud service definition. CloudServiceName: $CloudServiceName"
+
+catch [System.Exception] 
+{ 
+    Write-Error "======"
+    Write-Error $_.Exception.ToString()
+    exit 1 
+}
